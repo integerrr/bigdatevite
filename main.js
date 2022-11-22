5389e0aa8a133e06364447a5b1d6dcf95abba03f
integer <on9integer@pm.me>
2022-11-22 18:27:16 +0000

    init

---
+++ main.js
@@ -0,0 +1,188 @@
+import './style.css'
+import { TempusDominus } from '@eonasdan/tempus-dominus';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+  <div id="input">
+
+  <div
+      class = "input-group log-event"
+      id = "datetimepicker1"
+      data-td-target-input = "nearest"
+      data-td-target-toogle = "nearest"
+      >
+      <input
+          id="datetimepicker1Input"
+          type="text"
+          class="form-control"
+          placeholder=""
+          data-td-target="#datetimepicker1"
+          />
+      <span
+          class="input-group-text"
+          data-td-input="#datetimepicker1"
+          data-td-toogle="datatimepicker"
+          >
+          <span class="fa-solid fa-calendar"></span>
+      </span>
+  </div>
+
+
+  <button type="button" class="btn btn-primary" id="dateTimePickerReset"
+      style="margin: 20px;">Reset</button>
+  <!-- <h3 id="isoTime">a</h3> -->
+  <!-- <h1 id="convertedDate">b</h1> -->
+
+  <div id="output">
+      
+      <div class="input-group mb-3">
+          <input id="outputTextfield" type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="button-addon2">
+          <button class="btn btn-outline-light" type="button" id="outputCopyButton">
+              <span class="fa-regular fa-copy"></span>
+          </button>
+      </div>
+      
+  </div>
+  
+</div>
+`
+
+const TIME_TYPE = {
+  year: 0,
+  month: 1,
+  day: 1,
+  hour: 1,
+  minute: 2,
+  second: 2,
+};
+
+let realTimeMode = true;
+let realTimeClock = null;
+
+const picker = new TempusDominus(document.getElementById('datetimepicker1'), {
+  display: {
+    sideBySide: true,
+  },
+});
+
+/**
+ * @param {number} num
+ * @param {number} type
+ */
+function convertArabicNumbertoCantoNumber(num, type) {
+  const CANTO_NUMS = ['零', '壹', '貳', '參', '肆', '伍', '陸', '柒', '捌', '玖'];
+
+  // year
+  if (type === 0) {
+    return CANTO_NUMS[Math.floor(num / 1000)]
+    + CANTO_NUMS[Math.floor((num % 1000) / 100)]
+    + CANTO_NUMS[Math.floor((num % 100) / 10)]
+    + CANTO_NUMS[Math.floor(num % 10)];
+  }
+
+  // month, day, hour || minute, second
+  if (type === 1 || type === 2) {
+    if (num === 0) return '零';
+    if (num < 10) return type === 1 ? CANTO_NUMS[num] : `零${CANTO_NUMS[num]}`;
+    if (num < 20) return num === 10 ? '拾' : `拾${CANTO_NUMS[num % 10]}`;
+    if (num < 100) {
+      return (num % 10 === 0)
+        ? `${CANTO_NUMS[Math.floor(num / 10)]}拾`
+        : `${CANTO_NUMS[Math.floor(num / 10)]}拾${CANTO_NUMS[num % 10]}`;
+    }
+  }
+  return '???';
+}
+
+/**
+ * @param {Date} now
+ */
+function displayIsoDate(now) {
+  // document.getElementById('isoTime').innerHTML = now.toLocaleString();
+  document.getElementById('datetimepicker1Input').placeholder = now.toLocaleString();
+}
+
+/**
+ * @param {Date} now
+ */
+function displayCantoDate(now) {
+  let convertedTimeString = '';
+  const ampm = now.getHours() >= 12 ? '下午' : '上午';
+  const realHour = now.getHours() > 12
+    ? now.getHours() - 12
+    : now.getHours();
+
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getFullYear(), TIME_TYPE.year)}年`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getMonth() + 1, TIME_TYPE.month)}月`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getDate(), TIME_TYPE.day)}日`;
+  convertedTimeString += `${ampm + convertArabicNumbertoCantoNumber(realHour, TIME_TYPE.hour)}時`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getMinutes(), TIME_TYPE.minute)}分`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getSeconds(), TIME_TYPE.second)}秒`;
+
+  // document.getElementById('convertedDate').innerHTML = convertedTimeString;
+  document.getElementById('outputTextfield').value = convertedTimeString;
+}
+
+/**
+ * @param {import("@eonasdan/tempus-dominus").DateTime} pickedTime
+ */
+function displayPickedTime(pickedTime) {
+  displayIsoDate(pickedTime);
+  displayCantoDate(pickedTime);
+}
+
+function displayCurrentTime() {
+  const time = new Date();
+
+  displayIsoDate(time);
+  displayCantoDate(time);
+}
+
+function runDecoupledClock() {
+  clearInterval(realTimeClock);
+  const pickedTime = picker.dates.lastPicked;
+  pickedTime.setSeconds(0);
+  displayPickedTime(pickedTime);
+}
+
+function runClock() {
+  if (realTimeMode) {
+    realTimeClock = setInterval(() => {
+      displayCurrentTime();
+    }, 1000);
+  } else {
+    runDecoupledClock();
+  }
+}
+
+window.onload = () => {
+  const dateTimePickerInput = document.getElementById('datetimepicker1Input');
+  dateTimePickerInput.addEventListener('change', () => {
+    const pickedTime = picker.dates.lastPicked;
+    realTimeMode = pickedTime === undefined;
+    runClock();
+  });
+
+  document.getElementById('dateTimePickerReset').addEventListener('click', () => {
+    picker.dates.clear();
+    dateTimePickerInput.value = '';
+    realTimeMode = true;
+    displayCurrentTime();
+    runClock();
+  });
+
+  document.getElementById('outputCopyButton').addEventListener('click', () => {
+    const outputText = document.getElementById('outputTextfield').value;
+    const copyContent = async () => {
+      try {
+        await navigator.clipboard.writeText(outputText);
+        console.log(`copied "${outputText}"`);
+      } catch (error) {
+        console.error('cannot copy', error);
+      }
+    };
+  });
+
+  displayCurrentTime();
+  runClock();
+};
