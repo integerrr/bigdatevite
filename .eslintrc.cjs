309a4e9c585d5b248b1beaa451df7945c0bb39c2
integer <7279624+integerrr@users.noreply.github.com>
2022-12-11 13:28:26 +0000

    formatting naming changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 /* eslint-disable no-undef */
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': 'eslint:recommended',
-	'overrides': [
-	],
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'warn',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
--- main.js
+++ main.js
@@ -49,12 +49,12 @@ document.querySelector('#app').innerHTML = `
 `;
 
 const TIME_TYPE = {
-	year: 0,
-	month: 1,
-	day: 1,
-	hour: 1,
-	minute: 2,
-	second: 2,
+  year: 0,
+  month: 1,
+  day: 1,
+  hour: 1,
+  minute: 2,
+  second: 2,
 };
 
 let realTimeMode = true;
@@ -62,11 +62,11 @@ let realTimeClock = null;
 let useSecond = true;
 
 const picker = new TempusDominus(document.getElementById('datetimepicker1'), {
-	display: {
-		components: {
-			seconds: true,
-		},
-	},
+  display: {
+    components: {
+      seconds: true,
+    },
+  },
 });
 
 /**
@@ -74,152 +74,152 @@ const picker = new TempusDominus(document.getElementById('datetimepicker1'), {
  * @param {number} type
  */
 function convertArabicNumbertoCantoNumber(num, type) {
-	const CANTO_NUMS = ['零', '壹', '貳', '參', '肆', '伍', '陸', '柒', '捌', '玖'];
+  const CANTO_NUMS = ['零', '壹', '貳', '參', '肆', '伍', '陸', '柒', '捌', '玖'];
 
-	// year
-	if (type === 0) {
-		return CANTO_NUMS[Math.floor(num / 1000)]
+  // year
+  if (type === 0) {
+    return CANTO_NUMS[Math.floor(num / 1000)]
       + CANTO_NUMS[Math.floor((num % 1000) / 100)]
       + CANTO_NUMS[Math.floor((num % 100) / 10)]
       + CANTO_NUMS[Math.floor(num % 10)];
-	}
+  }
 
-	// month, day, hour || minute, second
-	if (type === 1 || type === 2) {
-		if (num === 0) return '零';
-		if (num < 10) return type === 1 ? CANTO_NUMS[num] : `零${CANTO_NUMS[num]}`;
-		if (num < 20) return num === 10 ? '拾' : `拾${CANTO_NUMS[num % 10]}`;
-		if (num < 100) {
-			return (num % 10 === 0)
-				? `${CANTO_NUMS[Math.floor(num / 10)]}拾`
-				: `${CANTO_NUMS[Math.floor(num / 10)]}拾${CANTO_NUMS[num % 10]}`;
-		}
-	}
-	return '???';
+  // everything else
+  if (type === 1 || type === 2) {
+    if (num === 0) return '零';
+    if (num < 10) return type === 1 ? CANTO_NUMS[num] : `零${CANTO_NUMS[num]}`;
+    if (num < 20) return num === 10 ? '拾' : `拾${CANTO_NUMS[num % 10]}`;
+    if (num < 100) {
+      return (num % 10 === 0)
+        ? `${CANTO_NUMS[Math.floor(num / 10)]}拾`
+        : `${CANTO_NUMS[Math.floor(num / 10)]}拾${CANTO_NUMS[num % 10]}`;
+    }
+  }
+  return '???';
 }
 
 /**
- * @param {Date} now
+ * @param {Date} time
  */
-function displayIsoDate(now) {
-	document.getElementById('datetimepicker1Input').placeholder = now.toLocaleString();
+function displayIsoDate(time) {
+  document.getElementById('datetimepicker1Input').placeholder = time.toLocaleString();
 }
 
 /**
- * @param {Date} now
+ * @param {Date} time
  */
-function displayCantoDate(now) {
-	let convertedTimeString = '';
-	const ampm = now.getHours() >= 12 ? '下午' : '上午';
-	const realHour = now.getHours() > 12
-		? now.getHours() - 12
-		: now.getHours();
+function displayCantoDate(time) {
+  let convertedTimeString = '';
+  const ampm = time.getHours() >= 12 ? '下午' : '上午';
+  const realHour = time.getHours() > 12
+    ? time.getHours() - 12
+    : time.getHours();
 
-	convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getFullYear(), TIME_TYPE.year)}年`;
-	convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getMonth() + 1, TIME_TYPE.month)}月`;
-	convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getDate(), TIME_TYPE.day)}日`;
-	convertedTimeString += `${ampm + convertArabicNumbertoCantoNumber(realHour, TIME_TYPE.hour)}時`;
-	convertedTimeString += `${convertArabicNumbertoCantoNumber(now.getMinutes(), TIME_TYPE.minute)}分`;
-	convertedTimeString += useSecond
-		? `${convertArabicNumbertoCantoNumber(now.getSeconds(), TIME_TYPE.second)}秒`
-		: '';
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(time.getFullYear(), TIME_TYPE.year)}年`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(time.getMonth() + 1, TIME_TYPE.month)}月`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(time.getDate(), TIME_TYPE.day)}日`;
+  convertedTimeString += `${ampm + convertArabicNumbertoCantoNumber(realHour, TIME_TYPE.hour)}時`;
+  convertedTimeString += `${convertArabicNumbertoCantoNumber(time.getMinutes(), TIME_TYPE.minute)}分`;
+  convertedTimeString += useSecond
+    ? `${convertArabicNumbertoCantoNumber(time.getSeconds(), TIME_TYPE.second)}秒`
+    : '';
 
-	document.getElementById('outputTextfield').value = convertedTimeString;
+  document.getElementById('outputTextfield').value = convertedTimeString;
 }
 
 /**
  * @param {import("@eonasdan/tempus-dominus").DateTime} pickedTime
  */
 function displayPickedTime(pickedTime) {
-	displayIsoDate(pickedTime);
-	displayCantoDate(pickedTime);
+  displayIsoDate(pickedTime);
+  displayCantoDate(pickedTime);
 }
 
 function displayCurrentTime() {
-	const time = new Date();
+  const now = new Date();
 
-	displayIsoDate(time);
-	displayCantoDate(time);
+  displayIsoDate(now);
+  displayCantoDate(now);
 }
 
 function runDecoupledClock() {
-	clearInterval(realTimeClock);
-	console.log('clock decoupled');
+  clearInterval(realTimeClock);
+  console.log('clock decoupled');
 
-	const pickedTime = picker.dates.lastPicked;
-	console.log(`manually picked time is ${pickedTime}`);
-	displayPickedTime(pickedTime);
+  const pickedTime = picker.dates.lastPicked;
+  console.log(`manually picked time is ${pickedTime}`);
+  displayPickedTime(pickedTime);
 }
 
 function runClock() {
-	if (realTimeMode) {
-		realTimeClock = setInterval(() => {
-			displayCurrentTime();
-		}, 1000);
-		console.log(`spawned RT clock interval timer with ID: ${realTimeClock}`);
-	} else {
-		runDecoupledClock();
-	}
+  if (realTimeMode) {
+    realTimeClock = setInterval(() => {
+      displayCurrentTime();
+    }, 1000);
+    console.log(`spawned RT clock interval timer with ID: ${realTimeClock}`);
+  } else {
+    runDecoupledClock();
+  }
 }
 
 function returnCopyBtnBackToOriginal() {
-	setTimeout(() => {
-		document.getElementById('output').querySelector('span').className = 'fa-regular fa-copy';
-	}, 1500);
+  setTimeout(() => {
+    document.getElementById('output').querySelector('span').className = 'fa-regular fa-copy';
+  }, 1500);
 }
 
 async function copyContent(outputText) {
-	try {
-		await navigator.clipboard.writeText(outputText);
-		console.log(`copied "${outputText}"`);
-		document.getElementById('output').querySelector('span').className = 'fa-solid fa-check';
-		returnCopyBtnBackToOriginal();
-	} catch (error) {
-		console.error('cannot copy', error);
-		document.getElementById('output').querySelector('span').className = 'fa-solid fa-xmark';
-		returnCopyBtnBackToOriginal();
-	}
+  try {
+    await navigator.clipboard.writeText(outputText);
+    console.log(`copied "${outputText}"`);
+    document.getElementById('output').querySelector('span').className = 'fa-solid fa-check';
+    returnCopyBtnBackToOriginal();
+  } catch (error) {
+    console.error('cannot copy, ', error);
+    document.getElementById('output').querySelector('span').className = 'fa-solid fa-xmark';
+    returnCopyBtnBackToOriginal();
+  }
 }
 
 window.onload = () => {
-	const dateTimePickerInput = document.getElementById('datetimepicker1Input');
-	dateTimePickerInput.addEventListener('change', () => {
-		const pickedTime = picker.dates.lastPicked;
-		console.log(pickedTime);
-		realTimeMode = pickedTime === undefined;
-		runClock();
-	});
+  const dateTimePickerInput = document.getElementById('datetimepicker1Input');
+  dateTimePickerInput.addEventListener('change', () => {
+    const pickedTime = picker.dates.lastPicked;
+    console.log(pickedTime);
+    realTimeMode = pickedTime === undefined;
+    runClock();
+  });
 
-	document.getElementById('dateTimePickerReset').addEventListener('click', () => {
-		// button becomes useless if inside conditional and user needs to clear the textbox
-		dateTimePickerInput.value = '';
+  document.getElementById('dateTimePickerReset').addEventListener('click', () => {
+    // button becomes useless if inside conditional and user needs to clear the textbox
+    dateTimePickerInput.value = '';
     
-		// button should only work when clock isn't already running real time,
-		// otherwise will spawn too many `setInterval`s via runClock()
-		if (!realTimeMode) {
-			// clearing picker must be inside conditional otherwise you get more
-			// setIntervals for whatever reason
-			picker.dates.clear();
-			realTimeMode = true;
-			displayCurrentTime();
-			runClock();
-		}
-	});
+    // button should only work when clock isn't already running real time,
+    // otherwise will spawn too many `setInterval`s via runClock()
+    if (!realTimeMode) {
+      // clearing picker must be inside conditional otherwise you get more
+      // setIntervals for whatever reason
+      picker.dates.clear();
+      realTimeMode = true;
+      displayCurrentTime();
+      runClock();
+    }
+  });
 
-	document.getElementById('outputCopyButton').addEventListener('click', () => {
-		const outputText = document.getElementById('outputTextfield').value;
-		copyContent(outputText);
-	});
+  document.getElementById('outputCopyButton').addEventListener('click', () => {
+    const outputText = document.getElementById('outputTextfield').value;
+    copyContent(outputText);
+  });
 
-	document.getElementById('useSecondSwitch').addEventListener('change', () => {
-		useSecond = !useSecond;
-		if (realTimeMode) {
-			displayCurrentTime();
-		} else {
-			displayPickedTime(picker.dates.lastPicked);
-		}
-	});
+  document.getElementById('useSecondSwitch').addEventListener('change', () => {
+    useSecond = !useSecond;
+    if (realTimeMode) {
+      displayCurrentTime();
+    } else {
+      displayPickedTime(picker.dates.lastPicked);
+    }
+  });
 
-	displayCurrentTime();
-	runClock();
+  displayCurrentTime();
+  runClock();
 };
